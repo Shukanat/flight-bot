b25d046562f955ae85d9952e652726bc0eadca66
Vitali <vitali.shchutski@dataimpact.io>
2022-07-25 20:55:15 +0200

    app is migrated to fastapi

--- app.py
+++ app.py
@@ -1,24 +1,12 @@
-# Copyright (c) Microsoft Corporation. All rights reserved.
-# Licensed under the MIT License.
+from fastapi import FastAPI, Request
+from fastapi.responses import JSONResponse
 
-"""
-This sample shows how to create a bot that demonstrates the following:
-- Use [LUIS](https://www.luis.ai) to implement core AI capabilities.
-- Implement a multi-turn conversation using Dialogs.
-- Handle user interruptions for such things as `Help` or `Cancel`.
-- Prompt for and validate requests for information from the user.
-"""
-from http import HTTPStatus
-
-from aiohttp import web
-from aiohttp.web import Request, Response, json_response
 from botbuilder.core import (
     BotFrameworkAdapterSettings,
     ConversationState,
     MemoryStorage,
     UserState,
 )
-from botbuilder.core.integration import aiohttp_error_middleware
 from botbuilder.schema import Activity
 
 from config import DefaultConfig
@@ -29,49 +17,29 @@ from adapter_with_error_handler import AdapterWithErrorHandler
 from flight_booking_recognizer import FlightBookingRecognizer
 
 CONFIG = DefaultConfig()
-
-# Create adapter.
-# See https://aka.ms/about-bot-adapter to learn more about how bots work.
 SETTINGS = BotFrameworkAdapterSettings(CONFIG.APP_ID, CONFIG.APP_PASSWORD)
-
-# Create MemoryStorage, UserState and ConversationState
 MEMORY = MemoryStorage()
 USER_STATE = UserState(MEMORY)
 CONVERSATION_STATE = ConversationState(MEMORY)
-
-# Create adapter.
-# See https://aka.ms/about-bot-adapter to learn more about how bots work.
 ADAPTER = AdapterWithErrorHandler(SETTINGS, CONVERSATION_STATE)
-
-# Create dialogs and Bot
 RECOGNIZER = FlightBookingRecognizer(CONFIG)
 BOOKING_DIALOG = BookingDialog()
 DIALOG = MainDialog(RECOGNIZER, BOOKING_DIALOG)
 BOT = DialogAndWelcomeBot(CONVERSATION_STATE, USER_STATE, DIALOG)
 
 
-# Listen for incoming requests on /api/messages.
-async def messages(req: Request) -> Response:
-    # Main bot message handler.
-    if "application/json" in req.headers["Content-Type"]:
+app = FastAPI()
+
+@app.post("/api/messages")
+async def messages(req: Request):
+    if "application/json" in req.headers["content-type"]:
         body = await req.json()
+        auth_header = req.headers["authorization"] if "authorization" in req.headers else ""
     else:
-        return Response(status=HTTPStatus.UNSUPPORTED_MEDIA_TYPE)
-
+        return JSONResponse(status_code=415, content={"message": "Unsupported media type"})
+    
     activity = Activity().deserialize(body)
-    auth_header = req.headers["Authorization"] if "Authorization" in req.headers else ""
-
     response = await ADAPTER.process_activity(activity, auth_header, BOT.on_turn)
     if response:
-        return json_response(data=response.body, status=response.status)
-    return Response(status=HTTPStatus.OK)
-
-
-APP = web.Application(middlewares=[aiohttp_error_middleware])
-APP.router.add_post("/api/messages", messages)
-
-if __name__ == "__main__":
-    try:
-        web.run_app(APP, host="localhost", port=CONFIG.PORT)
-    except Exception as error:
-        raise error
+        return JSONResponse(status_code=response.status, content=response.body)
+    return JSONResponse(status_code=200, content={'message': 'OK'})
